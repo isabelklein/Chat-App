55e8345a211a68d91e23d84b405b2ab39339f68c
System Administrator <root@D-10-18-212-134.dhcp4.washington.edu>
2019-07-03 11:31:58 -0700

    Edit chat app

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ import './App.css';
 import Logo from './Logo.png'
 import TextInput from './TextInput'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <img src={Logo} className="Logo" alt=""/>
-        Chatter
-      </header>
-      <div className="ChatLeft">
-          Hey! How are you?
-      </div>
-      <div className="ChatRight">
-        Hi! Nice to hear from you! I'm great! hbu?
-      </div>
-      <TextInput />
-    </div> 
-  );
-}
+class App extends React.Component {
+  
+  state={
+    messages:[]
+  }
 
+  sendMessage = (m) => {
+    var messages = [...this.state.messages, m]
+    this.setState({messages})
+  }
+
+  render() {
+    console.log(this.state.messages)
+    return (
+      <div className="App">
+        <header className="header">
+          <img src={Logo} className="Logo" alt="" />
+          Chatter
+      </header>
+        <TextInput sendMessage={this.sendMessage} />
+      </div>
+    );
+  }
+}
 export default App;
--- src/TextInput.js
+++ src/TextInput.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import arrow from './arrow.png'
+import { FiSend } from "react-icons/fi";
 
-class TextInput extends React.Component {
-    render(){
+class TextInput extends React.Component { /*creates text input box and send button*/
+    state = {
+        text: "",
+    }
+
+    send = () => {
+        this.props.sendMessage(this.state.text)
+        this.setState({ text: "" })
+    }
+
+    keypress = (e) => {
+        if(e.key==='Enter'){
+            this.send()
+        }
+    }
+
+    render() {
+        var {messages} = this.state
+        console.log(messages)
         return (<div className="text-input">
-            <input placeholder=" type a message..."/>
-            <button>
-            <img src={arrow} className="arrow" alt=""/>
+            <input value={this.state.text}
+                placeholder="Write your message here..."
+                onChange={e => this.setState({ text: e.target.value })}
+                onKeyPress={this.keyPress}
+            />
+            <button disabled={!this.state.text}
+                onClick={this.send}>
+                <FiSend style={{ height: 15, width: 15 }} />
             </button>
         </div>)
     }
