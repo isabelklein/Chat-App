036136c581214431d7903ff26885b26409d5d378
System Administrator <root@Isabels-MacBook-Pro-2.local>
2019-07-07 20:27:05 -0700

    add name to message

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import Logo from './Logo.png'
 import TextInput from './TextInput'
+import NamePicker from './NamePicker'
 
 class App extends React.Component {
 
-  gotMessage = (m) => {
-    var messages = [...this.state.messages, m]
-    this.setState({ messages })
+  state = {
+    messages: [],
+    name: '',
+    editName: false,
+  }
+
+  gotMessage = (text) => {
+    var newMessagesArray = [...this.state.messages, text]
+    this.setState({ messages: newMessagesArray })
   }
 
   render() {
@@ -17,10 +24,19 @@ class App extends React.Component {
         <header className="header">
           <img src={Logo} className="Logo" alt="" />
           Chatter
-      </header>
+          <NamePicker
+            name={this.state.name}
+            editName={this.state.editName}
+            changeName={name => this.setState({ name })}
+            setEditName={editName => this.setState({ editName })}
+          />
+        </header>
         <main className="messages">
           {messages.map((m, i) => {
             return (<div key={i} className="bubble-wrap">
+              <div className="bubble-name">
+                <span>{this.state.name}</span>
+              </div>
               <div className="bubble">
                 <span>{m}</span>
               </div>
@@ -32,4 +48,5 @@ class App extends React.Component {
     );
   }
 }
+
 export default App;
